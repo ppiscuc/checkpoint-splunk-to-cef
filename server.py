5f1a8e3b975d363e8f0b3a9fdd887f76ffef9365
Nobelium <nobelium@proximus.com>
2017-12-14 16:11:40 +0100

    added license

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import datetime
 
 HOSTNAME = socket.gethostname()
 VENDOR = 'Check Point'
+OUTPUT = 'checkpoint.cef'
+OUTPUTFH = open(OUTPUT, 'a')
 
 class TCPHandler(socketserver.StreamRequestHandler):
     """handle the tcp requests
@@ -17,12 +19,17 @@ class TCPHandler(socketserver.StreamRequestHandler):
         self.data = self.rfile.readline().strip()
         print("{} wrote:".format(self.client_address[0]))
         print("raw: {}".format(self.data))
+        if not self.data.startswith(b'time'):
+            # print("not an CP event")
+            # skip
+            return
         tokmap = self.parseToTokens(self.data)
         print("tokens: {}".format(tokmap))
-        ceftokens = self.tokensToCEF(tokmap)
+        ceftokens = self.tokensToCEFTokens(tokmap)
         print("ceftokens: {}".format(ceftokens))
         cef = self.CEFTokensToCEF(ceftokens)
-        print("cef: {}".format(cef
+        print("cef: {}".format(cef))
+        OUTPUTFH.write(cef + '\n')
         #self.wfile.write(self.data.upper())
     
     def parseToTokens(self, binarydata):
@@ -43,25 +50,55 @@ class TCPHandler(socketserver.StreamRequestHandler):
 
     def tokensToCEFTokens(self, t):
         """covers a token map to a CEF event"""
-        deviceTime = datetime.datetime.fromtimestamp(int(t["time"])).strftime('%Y-%m-%d %H:%M:%S')
-        product = t["product"]
-        deviceEventClassID = t["action"]
-        name = t["action"]
-        severity = "5"
-        host =  HOSTNAME
-        cef = deviceTime + ' ' + host + ' CEF:1| ' + VENDOR + '|' + product + '|' + 'R80' + '|' + deviceEventClassID + '|' + name + '|' + severity + '|'
-        # pop the processed extensions
-        tokmap.pop('time', None)
-        tokmap.pop('product', None)
-        tokmap.pop('action', None)
-        
-        extensions = {}
-        extensions['deviceReceiptTime'] = deviceTime
-        extensions['name'] = self.OneOf(t
+        # Jan 18 11:07:53
+        deviceTime = datetime.datetime.fromtimestamp(int(t["time"])).strftime('%b %d %H:%M:%S')
+        # extensions
+        e = {}
+        e['deviceReceiptTime'] = deviceTime
+        e['name'] = self.OneOf(t.get('attack', ''),t.get('action',''),t.get('alert',''))
+        e['severity'] = '5'
+        e['product'] = e.get('product', 'VPN-1 & FireWall-1') # TODO
+        e['deviceEventClassID'] = e.get('action', '')
+        e['deviceInboundInterface'] = e.get('i/f_name', '') if e.get('i/f_dir',None) == "inbound" else ""
+        e['deviceOutboundInterface']= e.get('i/f_name', '') if e.get('i/f_dir', None) == "outbound" else ""
+        e['bytesIn'] = e.get('client_inbound_bytes', '0') if e.get('i/f_dir', None) == "inbound" else e.get('server_inbound_bytes', '0')
+        e['bytesOut'] = e.get('client_outbound_bytes', '0') if e.get('i/f_dir', None) == "outbound" else e.get('server_outbound_bytes', '0')
+        e['deviceCustomString5Label'] = 'Vlan ID' if e.get('VLAN ID', None) is not None else 'Total bytes'
+        e['deviceCustomString5'] = e.get('Vlan ID', '') if e.get('Vlan ID', None) is not None else e.get('bytes', '')
+        # deviceDirection
+        e['deviceAddress'] = e.get('orig', '')
+        e['deviceFacility'] = self.OneOf(e.get('orig_name', ''), e.get('product_family', ''))
+        alerta = e.get('alert', '') if e.get('alert','') == 'alert' else ''
+        e['deviceAction'] = self.OneOf(e.get('action', ''), e.get('scan result', ''), e.get('spyware_action',''), alerta)
+        e['deviceSeverity'] = self.OneOf(e.get('action',''), e.get('scan result', ''), e.get('spyware_action',''))
+        leg1  = self.OneOf(e.get('attack', ''), e.get('action', ''), e.get('spyware_action',''), e.get('alert',''), e.get('integrity_av_event',''))
+        leg2 = self.OneOf(e.get('ICMP',''), e.get('reason',''), e.get('reason:, sys_msgs',''), e.get('message_info',''), e.get('IKE log:',''))
+        leg3 = self.OneOf(e.get('attack', ''), e.get('action', ''), e.get('spyware_action',''), e.get('alert',''), e.get('integrity_av_event',''))
+        e['deviceEventClassID'] = 'authcrypt|' + leg2 if leg1 == 'authcrypt' else leg3
 
+        return e
+
+
+    def CEFTokensToCEF(self, ct):
+        """puts the cef tokens in their place"""
+        # header
+        host = HOSTNAME
+        cef = ct['deviceReceiptTime'] + ' ' + host + ' CEF:1|' + VENDOR + '|' + ct['product'] + '|' + 'R80' + '|' + ct['deviceEventClassID'] + '|' + ct['name'] + '|' + ct['severity'] + '|'
+        # we should pop the used keys
+        kvs = []
+        for key, value in ct.items():
+            kv = key + '=' + value
+            kvs.append(kv)
+        extensions = str.join('|', kvs)
+        cef = cef + extensions
         return cef
 
 
+    def OneOf(self, *args):
+        for arg in args:
+            if arg is not None and arg != '':
+                return arg
+        return ''
 
 
 if __name__ == "__main__":
@@ -69,4 +106,8 @@ if __name__ == "__main__":
     # create the server and bind
     server = socketserver.TCPServer((HOST, PORT), TCPHandler)
     # wait until ctrl+c
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except Exception as e:
+        OUTPUTFH.close()
+        print('exception:', e)
